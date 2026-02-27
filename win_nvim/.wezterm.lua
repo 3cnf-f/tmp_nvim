68a59960cad37f008bd0ac98fd1e115045dcfa2b
3cnf-f <154888836+3cnf-f@users.noreply.github.com>
2026-02-27 16:21:02 +0100

    Add key binding for F12 to send specific strings

---
+++ win_nvim/.wezterm.lua
@@ -0,0 +1,8 @@
+config.keys = {
+  -- ... your other stuff ...
+  {
+    key = 'F12',  -- pick whatever key you like—F12's free usually
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SendString '   \n',
+  },
+}
