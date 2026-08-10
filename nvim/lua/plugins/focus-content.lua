c540528c074c42dedc5b58e390748bea4d522b42
3cnf-f <154888836+3cnf-f@users.noreply.github.com>
2026-08-10 18:27:07 +0000

    f_acp: no args

---
+++ nvim/lua/plugins/focus-content.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    dir = "/root/py_projects/focus-content.nvim",
+    name = "focus-context.nvim",
+    config = function()
+      require("focus_context").setup()
+    end,
+  },
+}
